237ac5b611749fb916f7bfbbb159b95fbf889c51
Angel Gallego <angelgb1990@gmail.com>
2024-10-05 23:54:16 +0000

    boton añadido sin refresh pag

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
+import { arrow } from "@popperjs/core";
 
-window.onload = function() {
-  document.querySelector("#excusa").innerHTML = genExcusa();
-};
+const genButton = document.getElementById("genButton");
+const excusa = document.getElementById("excusa");
+
 function genExcusa() {
   let pro = ["El", "Mi", "Su", "Aquel", "Ese"];
   let quien = [" Murlok", " Tauren", " Fúrbolg", " Jabaespín", " Abisario"];
@@ -32,18 +34,30 @@ function genExcusa() {
     " de tu zurrón",
     " de la tienda"
   ];
+  function x(arr) {
+    return Math.floor(Math.random() * arr.length);
+    // Limpiar codigo
+    // let pro1 = Math.floor(Math.random() * pro.length);
+    // let quien1 = Math.floor(Math.random() * quien.length);
+    // let verbo1 = Math.floor(Math.random() * verbo.length);
+    // let objetivo1 = Math.floor(Math.random() * objetivo.length);
+    // let donde1 = Math.floor(Math.random() * donde.length);
+  }
 
-  let pro1 = Math.floor(Math.random() * pro.length);
-  let quien1 = Math.floor(Math.random() * quien.length);
-  let verbo1 = Math.floor(Math.random() * verbo.length);
-  let objetivo1 = Math.floor(Math.random() * objetivo.length);
-  let donde1 = Math.floor(Math.random() * donde.length);
+  let genExcusa =
+    pro[x(pro)] +
+    quien[x(quien)] +
+    verbo[x(verbo)] +
+    objetivo[x(objetivo)] +
+    donde[x(donde)];
 
-  return (
-    pro[pro1] +
-    quien[quien1] +
-    verbo[verbo1] +
-    objetivo[objetivo1] +
-    donde[donde1]
-  );
+  // pro([pro1] * pro.length) +
+  // quien[quien1] +
+  // verbo[verbo1] +
+  // objetivo[objetivo1] +
+  // donde[donde1];
+  excusa.textContent = genExcusa;
 }
+// Como llamar a una funcion desde un boton
+genButton.addEventListener("click", genExcusa);
+genExcusa();
